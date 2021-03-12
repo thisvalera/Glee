70fcb10d7fa1f0289164a0b8914a06248ce67243
valeriy <valleerroonn4iksho@gmail.com>
2021-03-12 15:36:04 +0200

    page-blog done

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,12 @@
 $(function () {
 
-  
+  $('.pagination__link').on('click', function () {
+    $('.pagination__link').removeClass('pagination__link--active');
+    $(this).toggleClass('pagination__link--active');
+});
+
+
+
   $('.slider-shop__inner').slick({
     dots: true,
     arrows: false,
@@ -9,16 +15,16 @@ $(function () {
     fade: true,
     autoplay: true,
   });
-  
-  var mixerProducts = mixitup('.products-gallery__gallery', {
-    selectors: {
-      control: '.products-gallery__btn'
-    }
-  });
-  
-  var mixerDesign = mixitup('.design-gallery__items', {
-    selectors: {
-      control: '.design__btn'
-    }
-  });
+
+  // var mixerProducts = mixitup('.products-gallery__gallery', {
+  //   selectors: {
+  //     control: '.products-gallery__btn'
+  //   }
+  // });
+
+  // var mixerDesign = mixitup('.design-gallery__items', {
+  //   selectors: {
+  //     control: '.design__btn'
+  //   }
+  // });
 });
