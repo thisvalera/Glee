9e3d83f59bb9f3aec096d85833a9425fadc00ba0
valeriy <valleerroonn4iksho@gmail.com>
2021-03-23 16:26:34 +0200

    added product-cart top-page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,30 @@
 $(function () {
 
+  $('.product-tabs__top-item').on('click', function(e){
+    e.preventDefault()
+  })
+
+  $('.product-cart__content-num').styler();
+
+  $('.product-cart__slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.product-cart__sub-slider',
+    draggable: false,
+  });
+  $('.product-cart__sub-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.product-cart__slider',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true,
+    vertical: true,
+    draggable: false,
+  });
+
     $(".recent-product__star").rateYo({
       starWidth: "10px",
       normalFill: "#d6d6d6",
@@ -9,7 +34,7 @@ $(function () {
      starSvg: '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star" class="svg-inline--fa fa-star fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill-rule:nonzero" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>',
     });
 
-    $(".second-product__star").rateYo({
+    $(".second-product__star, .product-cart__star").rateYo({
       starWidth: "17px",
       normalFill: "#d6d6d6",
       ratedFill: "#ffcc00",
@@ -34,7 +59,7 @@ $(function () {
   });
 
   $('.pagination__link').on('click', function () {
-    $('.pagination__link').removeClass('pagination__link--active');
+    $('.pagination__link, .product-tabs__top-item,').removeClass('pagination__link--active');
     $(this).toggleClass('pagination__link--active');
 });
 
